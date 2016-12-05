9d16da7d1f164225cfba730495f4583704e21942
seiji mendoza <srm5149@gmail.com>
2016-12-05 11:59:09 -0800

    updated routes for todo items

--- todo/app/controllers/todo_items_controller.rb
+++ todo/app/controllers/todo_items_controller.rb
@@ -1,9 +1,9 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, expect: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
-
     redirect_to @todo_list
   end
 
@@ -17,11 +17,19 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
 
   private
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :todo_lists do
-    resources :todo_items
-  end
 
-
+   resources :todo_lists do
+     resources :todo_items do
+       member do
+         patch :complete
+       end
+     end
+   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "todo_lists#index"
